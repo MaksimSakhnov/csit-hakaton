426508a254e880e6b4c834624daeecb870d8b5a9
Максим Сахнов <maxim.sahnov@satellite-soft.ru>
2024-02-23 21:59:01 +0400

    добавлен docker-compose для backend

--- components/backend/src/app.module.ts
+++ components/backend/src/app.module.ts
@@ -3,28 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import {TypeOrmModule} from "./db/typeorm.module";
 
 @Module({
   imports: [
+    ConfigModule,
+    TypeOrmModule,
     UserModule,
-    AuthModule,
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.js, .ts}']
-      }),
-      inject: [ConfigService],
-    })
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ components/backend/src/db/typeorm.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule as NestTypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+@Module({
+    imports: [
+        NestTypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: String(process.env.POSTGRES_USERNAME),
+            password: String(process.env.POSTGRES_PASSWORD),
+            database: process.env.POSTGRES_DATABASE,
+            entities: ['dist/modules/**/*.entity.js'],
+            synchronize: true,
+            // migrations: [ 'dist/db/migrations/**/*.js' ],
+            // cli: { migrationsDir: 'src/db/migrations' },
+        }),
+    ],
+})
+export class TypeOrmModule {}
