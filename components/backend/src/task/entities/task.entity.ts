5a7316341d4268d76760170153d298c355000dc2
Olga Lokuta <olokuta20j@gmail.com>
2024-02-25 08:49:21 +0400

    Разрешен конфликт

--- components/backend/src/task/entities/task.entity.ts
+++ components/backend/src/task/entities/task.entity.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, RelationId} from 'typeorm';
-=======
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
->>>>>>> cfeb4bf11015d09e8dc11319bd92f72c4eac5fed
 
 import { Course } from 'src/course/entities/course.entity';
 import { Attempt } from 'src/attempt/entities/attempt.entity';
@@ -25,16 +21,10 @@ export class Task {
     @Column()
     max_points: number
 
-<<<<<<< HEAD
-    @ManyToOne(()=>Course, {cascade:true, eager:true})
-    @JoinColumn({name:"courseId"})
-    course:Course
-=======
     @OneToMany((type) => Attempt, (attempt) => attempt.task)
     attempts?: Attempt[]
 
     @ManyToOne((type) => Course, { eager: true })
     @JoinColumn({ name: "courseId" })
     course: Course
->>>>>>> cfeb4bf11015d09e8dc11319bd92f72c4eac5fed
 }
